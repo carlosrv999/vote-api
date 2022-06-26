4fb94c6decd33f250d8766857ec06edd2d1b6156
Carlos Ramirez <carlosrv125@icloud.com>
2022-06-27 04:01:27 +0800

    added api gateway

--- vote.js
+++ vote.js
@@ -27,4 +27,24 @@ router.post('/votes', function(req, res) {
   });
 });
 
+router.get('/api/votes', function(req, res) {
+  pool.query(`select emoji_id, count(*) as vote_quantity from votes group by emoji_id order by vote_quantity desc;`, function (error, results, fields) {
+    if (error) {
+      res.status(500).send(error);
+    };
+    res.send(results);
+  });
+});
+
+router.post('/api/votes', function(req, res) {
+  console.log(req.body)
+  pool.query(`insert into votes (emoji_id, voting_date) values (${req.body.emoji_id}, CURRENT_TIMESTAMP);`, function (error, results, fields) {
+  // pool.query(`select emoji_id, count(*) as votes_for from votes group by emoji_id;`, function (error, results, fields) {
+    if (error) {
+      res.status(500).send(error);
+    };
+    res.send(results);
+  });
+});
+
 module.exports = router;
