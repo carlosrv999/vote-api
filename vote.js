97ea110307bf4c3bc82668a0ed85a69e859f49af
Carlos Ramirez <carlosrv125@icloud.com>
2022-07-13 23:48:17 -0500

    refactor code

--- vote.js
+++ vote.js
@@ -1,31 +1,28 @@
 import express from 'express'
-import { pool } from './index.js'
+import { querySelectVotes, queryInsertVote } from './queries.js'
 const router = express.Router()
 
-router.use(function timeLog(req, res, next) {
+router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
 })
 
-router.get('/vote', function(req, res) {
-  pool.query(`select emoji_id, count(*) as vote_quantity from votes group by emoji_id order by vote_quantity desc;`, function (error, results, fields) {
-    if (error) {
-      res.status(500).send(error)
-    }
+router.get('/vote', (req, res) => {
+  querySelectVotes((results) => {
     res.send(results)
+  }, (error) => {
+    res.status(500).send(error)
   })
 })
 
-router.post('/vote', function(req, res) {
-  console.log(req.body)
-  pool.query(`insert into votes (emoji_id, voting_date) values (?, CURRENT_TIMESTAMP);`, [req.body.emoji_id], function (error, results, fields) {
-    if (error) {
-      res.status(500).send(error)
-    }
+router.post('/vote', (req, res) => {
+  queryInsertVote(req, (results) => {
     res.send({
-      "result":"success",
+      "result": "success",
       "voted": req.body
     })
+  }, (error) => {
+    res.status(500).send(error)
   })
 })
 
